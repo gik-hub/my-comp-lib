29673274dcfc9778d2c51f6421f84cf73b50c490
gik-hub <gikandi95@gmail.com>
2020-08-24 10:35:26 +0300

    styled components

---
+++ src/components/Modals.js
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import { typeScale } from "../utils";
+import { Illustrations, CloseIcon } from "../assets";
+import { PrimaryButton } from "./Buttons";
+
+const ModalWrapper = styled.div`
+  width: 800px;
+  height: 580px;
+  box-shadow: 0 5 16px rgba(0, 0, 0, 0.2);
+  border-radius: 2px;
+  background-color: ${(props) => props.formElementBackground};
+  color: ${(props) => props.textOnFormElementBackground};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
+`;
+
+const SignUpHeader = styled.h3`
+  font-size: ${typeScale.header3};
+`;
+
+const SignUpText = styled.p`
+  font-size: ${typeScale.paragraph};
+  max-width: 70%;
+  text-align: center;
+`;
+
+const CloseModalButton = styled.button`
+  cursor: pointer;
+  background: none;
+  border: none;
+  position: absolute;
+  top: 40px;
+  right: 40px;
+  width: 24px;
+  height: 24px;
+  padding: 0;
+`;
+
+export const SignUpModal = () => {
+  return (
+    <ModalWrapper>
+      <img
+        src={Illustrations.SignUp}
+        alt="sign up for an acc"
+        arial-aria-hidden="true"
+      />
+      <SignUpHeader>Sign up!</SignUpHeader>
+      <SignUpText>
+        sign up today to get access to cool things bluh bluh bluh
+      </SignUpText>
+      <PrimaryButton>Sign up</PrimaryButton>
+      <CloseModalButton arial-label="Close Modal">
+        <CloseIcon />
+      </CloseModalButton>
+    </ModalWrapper>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import {
+  PrimaryButton,
+  SecondaryButton,
+  TertiaryButton,
+  SignUpModal,
+} from "./components";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { GlobalStyle, defaultTheme, darkTheme } from "./utils";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+  return (
+    <>
+      <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+        <PrimaryButton onClick={() => setUseDarkTheme(!useDarkTheme)}>
+          Toogle Theme
+        </PrimaryButton>
+        <div
+          style={{
+            background: useDarkTheme
+              ? defaultTheme.primaryColor
+              : darkTheme.primaryColor,
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          <SignUpModal />
+        </div>
+        <div>
+          normal => &nbsp;
+          <PrimaryButton>Hello world</PrimaryButton> &nbsp;
+          <SecondaryButton>Hello world</SecondaryButton> &nbsp;
+          <TertiaryButton>Hello world</TertiaryButton> &nbsp;
+        </div>
+        <div>
+          small disabled => &nbsp;
+          <PrimaryButton disabled modifiers={["small"]}>
+            Hello world
+          </PrimaryButton>{" "}
+          &nbsp;
+          <SecondaryButton disabled modifiers={["small"]}>
+            Hello world
+          </SecondaryButton>{" "}
+          &nbsp;
+          <TertiaryButton disabled modifiers={["small"]}>
+            Hello world
+          </TertiaryButton>{" "}
+          &nbsp;
+        </div>
+        {/* <PrimaryButton modifiers={["small", "warning"]}>
+          Hello world
+        </PrimaryButton>{" "}
+        <TertiaryButton modifiers={["small", "warning", "secondaryWarning"]}>
+          Hello world
+        </TertiaryButton>{" "} */}
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
